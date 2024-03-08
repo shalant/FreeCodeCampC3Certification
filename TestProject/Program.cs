1e60b2178740704af29cd665b6b4259e209bdb59
shalant <doug.rosenberg@gmail.com>
2024-03-07 21:14:40 -0600

    initial commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,88 @@
+﻿// int hero = 10;
+// int monster = 10;
+
+// Random dice = new Random();
+
+// do
+// {
+//     int roll = dice.Next(1, 11);
+//     monster -= roll;
+//     Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+//     if (monster <= 0) continue;
+
+//     roll = dice.Next(1, 11);
+//     hero -= roll;
+//     Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+// } while (hero > 0 && monster > 0);
+
+// Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+
+
+// string? readResult;
+// bool validEntry = false;
+// Console.WriteLine("Enter a string containing at least three characters:");
+// do
+// {
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         if (readResult.Length >= 3)
+//         {
+//             validEntry = true;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Your input is invalid, please try again.");
+//         }
+//     }
+// } while (validEntry == false);
+
+
+// capture user input in a string variable named readResult
+
+
+
+
+// string? readResult;
+
+// int numericValue = 0;
+// bool validNumber = false;
+
+// validNumber = int.TryParse(readResult, out numericValue);
+
+
+
+string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do{
+    readResult = Console.ReadLine();
+    if(readResult != null)
+    {
+        valueEntered = readResult;
+    }
+    validNumber = int.TryParse(valueEntered, out numValue);
+
+    if(validNumber == true)
+    {
+        if(numValue <= 5 || numValue >= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again.");
+    }
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+readResult = Console.ReadLine();
