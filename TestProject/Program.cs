bf1c5c7944c0ba30b7b13721c51bf365363ad986
shalant <doug.rosenberg@gmail.com>
2024-03-13 13:02:45 -0500

    complete 2 more challenges

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -54,35 +54,120 @@
 
 
 
-string? readResult;
-string valueEntered = "";
-int numValue = 0;
-bool validNumber = false;
+// string? readResult;
+// string valueEntered = "";
+// int numValue = 0;
+// bool validNumber = false;
 
-Console.WriteLine("Enter an integer value between 5 and 10");
+// Console.WriteLine("Enter an integer value between 5 and 10");
 
-do{
-    readResult = Console.ReadLine();
-    if(readResult != null)
+// do{
+//     readResult = Console.ReadLine();
+//     if(readResult != null)
+//     {
+//         valueEntered = readResult;
+//     }
+//     validNumber = int.TryParse(valueEntered, out numValue);
+
+//     if(validNumber == true)
+//     {
+//         if(numValue <= 5 || numValue >= 10)
+//         {
+//             validNumber = false;
+//             Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again.");
+//     }
+// } while (validNumber == false);
+
+// Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+// readResult = Console.ReadLine();
+
+
+
+
+
+// string[] roles = new string[] { "Administrator", "Manager", "User"};
+
+// Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+// string? userInput = Console.ReadLine();
+
+// if (userInput == roles[0] || userInput == roles[1] || userInput == roles[2])
+// {
+//     Console.WriteLine($"Your input value ({userInput}) has been accepted.");
+// }
+// else
+// {
+//     Console.WriteLine($"The role name that you entered, \"{userInput}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+   
+// }
+
+
+
+// string? readResult;
+// string roleName = "";
+// bool validEntry = false;
+
+// do
+// {
+//     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         roleName = readResult.Trim();
+//     }
+
+//     if(roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+//     {
+//         validEntry = true;
+//     }
+//     else 
+//     {
+//         Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+//     }
+// }
+// while (validEntry == false);
+
+// Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+// readResult = Console.ReadLine();
+
+
+
+// string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+
+
+
+string[] myStrings = new string[2] {"I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"};
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    while(periodLocation != -1)
     {
-        valueEntered = readResult;
-    }
-    validNumber = int.TryParse(valueEntered, out numValue);
+        mySentence = myString.Remove(periodLocation);
 
-    if(validNumber == true)
-    {
-        if(numValue <= 5 || numValue >= 10)
-        {
-            validNumber = false;
-            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again.");
-    }
-} while (validNumber == false);
+        myString = myString.Substring(periodLocation + 1);
 
-Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+        myString = myString.TrimStart();
 
-readResult = Console.ReadLine();
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
