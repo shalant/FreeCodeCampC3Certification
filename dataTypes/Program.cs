36ec6fa91e314ad54f520f7266be4fa43ce0aa2b
shalant <doug.rosenberg@gmail.com>
2024-03-17 21:56:32 -0500

    making progress

---
+++ dataTypes/Program.cs
@@ -0,0 +1,437 @@
+﻿// Console.WriteLine("Signed integral types:");
+
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types:");
+
+// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+
+// Console.WriteLine("");
+// Console.WriteLine("Floating point types:");
+// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+// int first = 2;
+// string second = "4";
+// int result = first + int.Parse(second);
+// Console.WriteLine(result);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = int.Parse(value1) * int.Parse(value2);
+// Console.WriteLine(result);
+
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+// string value = "102";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//    Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//    Console.WriteLine("Unable to report the measurement.");
+// }
+
+// string value = "bad";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//    Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//    Console.WriteLine("Unable to report the measurement.");
+// }
+// if (result > 0)
+//    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// decimal numericTotal = 0;
+// decimal result = 0;
+// string message = "";
+// foreach(var value in values) 
+// {
+//     if (decimal.TryParse(value, out result))
+//     {
+//         numericTotal += result;
+//     }
+//     else 
+//     {
+//         message += value;
+//     }
+// }
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {numericTotal}");
+
+// int value1 = 12;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
+// int result1;
+// decimal result2;
+// float result3;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+// result1 = Convert.ToInt32(value1 / value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// // Your code here to set result2
+// result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// // Your code here to set result3
+// result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] message = pangram.Split(" ");
+// foreach(var word in message)
+// {
+// Console.WriteLine(word);
+
+// }
+// string[] newMessage = new String[message.Length];
+// for(int i = 0; i < message.Length; i++)
+// {
+//     char[] letters = message[i].ToCharArray();
+//     Array.Reverse(letters);
+//     newMessage[i] = new string(letters);
+// }
+
+// string result = String.Join(" ", newMessage);
+// Console.WriteLine(result);
+
+// string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string[] orders = orderStream.Split(",");
+// Array.Sort(orders);
+// foreach(var order in orders)
+// {
+//     Console.Write(order);
+//     if(order.Length < 4)
+//     {
+//         Console.Write("  - error");
+//     }
+//     else if(order.Length > 4)
+//     {
+//         Console.Write("  - error");
+//     }
+//     Console.WriteLine();
+// };
+
+// decimal tax = .36785m;
+// Console.WriteLine($"Tax rate: {tax:P2}");
+
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+
+// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+// yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+// Console.WriteLine(yourDiscount);
+
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Shares: {productShares:N3} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
+
+// string input = "Pad this";
+// Console.WriteLine(input.PadLeft(12, '-'));
+//  Console.WriteLine(input.PadRight(12, '-'));
+
+// string paymentId = "769C";
+//  string payeeName = "Mr. Stephen Ortega";
+//  string paymentAmount = "$5,000.00";
+
+//  var formattedLine = paymentId.PadRight(6);
+//  formattedLine += payeeName.PadRight(24);
+//  formattedLine += paymentAmount.PadLeft(10);
+// Console.WriteLine("1234567890123456789012345678901234567890");
+//  Console.WriteLine(formattedLine);
+
+// string customerName = "Ms. Barros";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// // Your logic here
+// Console.WriteLine($"Dear {customerName},");
+// Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+// Console.WriteLine();
+// Console.WriteLine($"Currently you own {currentShares:N} shares at a return of {currentReturn:P2}.\n");
+// Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given you current columne, your potential profit would be {newProfit:C}");
+// Console.WriteLine();
+// Console.WriteLine("Here's a quick comparison:\n");
+
+// Console.WriteLine($"{currentProduct.PadRight(24)} {currentReturn:P2}.Padright(12) {currentProfit:C}");
+// Console.WriteLine();
+
+// string comparisonMessage = currentProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+// comparisonMessage += "\n";
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", newProfit).PadRight(10);
+
+// Console.WriteLine(comparisonMessage);
+
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// // The IndexOfAny() helper method requires a char array of characters. 
+// // You want to look for:
+
+// char[] openSymbols = { '[', '{', '(' };
+
+// // You'll use a slightly different technique for iterating through 
+// // the characters in the string. This time, use the closing 
+// // position of the previous iteration as the starting index for the 
+// //next open symbol. So, you need to initialize the closingPosition 
+// // variable to zero:
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1) break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     // To find the closingPosition, use an overload of the IndexOf method to specify 
+//     // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     // Finally, use the techniques you've already learned to display the sub-string:
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ");
+// message = message.Replace("-", "");
+// Console.WriteLine(message);
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int quantityStart = input.IndexOf(openSpan) + openSpan.Length;
+int quantityEnd = input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+// set output to input, replacing the trademark symbol with the registered trademark symbol
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+//remove opening div tag
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+//remove closing div and add "output:" to the the beginnging
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+
+
+
+
+
+
+
+
+
+
+// --------------ALGOS--------------------
+
+// Console.WriteLine("Please enter a first value");
+// string? value1 = Console.ReadLine();
+// int x;
+// Console.WriteLine("Please enter a second value");
+// string? value2 = Console.ReadLine();
+// int y;
+
+// int.TryParse(value1, out x);
+
+// int.TryParse(value2, out y);
+// int sum = x + y;
+
+// if(x == y)
+// {
+//     Console.WriteLine(3 * sum);
+// }
+// else
+// {
+//     Console.WriteLine($"The sum of the 2 numbers is {sum}");
+// }
+
+
+
+
